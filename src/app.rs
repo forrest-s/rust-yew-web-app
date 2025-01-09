207e45b5a8a42aca5d0f173159ad599a34417e6a
Forrest S <thewanderinwolves@gmail.com>
2025-01-08 22:53:57 -0800

    wip: added form in header with multiple inputs for todos.

--- src/app.rs
+++ src/app.rs
@@ -33,12 +33,36 @@ pub fn app() -> Html {
     };
 
     html! {
-        <main>
-            <input type="text" value={input.clone().to_string()} {oninput} />
-            <button{onclick}>{ "add todo" }</button>
-            <ul>
-                { for todos.iter().map(|todo| html! { <li>{ todo }</li> }) }
-            </ul>
-        </main>
+        <>
+            <header>
+                <form id="newBountyForm" name="newBounty">
+                    <label>{ "First Name" }</label>
+                    <input type="text" name="firstName" placeholder="First Name" />
+                    <label>{ "Last Name" }</label>
+                    <input type="text" name="lastName" placeholder="Last Name" />
+                    <label>{ "Living" }</label>
+                    <select type="text" name="living" placeholder="First Name" >
+                        <option>{ "is bounty alive" }</option>
+                        <option value="true">{ "True" }</option>
+                        <option value="false">{ "False" }</option>
+                    </select>
+                    <label>{ "Bounty Amount" }</label>
+                    <input type="text" name="bountyAmount" placeholder="Bounty Amount" />
+                    <label>{ "Type" }</label>
+                    <select>
+                        <option>{ "Jedi or Sith" }</option>
+                        <option value="jedi">{ "Jedi" }</option>
+                        <option value="sith">{ "Sith" }</option>
+                    </select>   
+                </form>
+            </header>
+            <main>
+                <input type="text" value={input.clone().to_string()} {oninput} />
+                <button{onclick}>{ "add todo" }</button>
+                <ul>
+                    { for todos.iter().map(|todo| html! { <li>{ todo }</li> }) }
+                </ul>
+            </main>
+        </>
     }
 }
